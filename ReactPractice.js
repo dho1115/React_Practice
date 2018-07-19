7c915c3260da03cfe2041a37fe5bd576e62a570c
DAVID HO <davidhorph@hotmail.com>
2018-07-19 14:59:54 -0500

    ...

--- ReactPractice.js
+++ ReactPractice.js
@@ -12,7 +12,7 @@ class LikeButton extends React.Component {
 
   render() {
     if (this.state.liked) {
-      return 'You liked this.';
+      return 'You liked comment number ' + this.props.commentID;
     }
 
     return e(
@@ -23,7 +23,13 @@ class LikeButton extends React.Component {
   }
 }
 
-const domContainer = document.querySelector("#LikeButton");
-ReactDOM.render(e(LikeButton),(domContainer));
-
-
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll('.like_button_container')
+  .forEach(domContainer => {
+    // Read the comment ID from a data-* attribute.
+    const commentID = parseInt(domContainer.dataset.commentid, 10);
+    ReactDOM.render(
+      e(LikeButton, { commentID: commentID }),
+      domContainer
+    );
+  });
