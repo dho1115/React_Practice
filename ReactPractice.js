40becb5a506bb868ef8b852b0be059e3353fdfb2
DAVID HO <davidhorph@hotmail.com>
2018-07-19 15:27:41 -0500

    tried to minify...

--- ReactPractice.js
+++ ReactPractice.js
@@ -1,5 +1,3 @@
-
-
 'use strict';
 
 const e = React.createElement;
@@ -12,7 +10,7 @@ class LikeButton extends React.Component {
 
   render() {
     if (this.state.liked) {
-      return 'You liked comment number ' + this.props.commentID;
+      return 'You liked this.';
     }
 
     return e(
@@ -21,15 +19,7 @@ class LikeButton extends React.Component {
       'Like'
     );
   }
-}
+} 
 
-// Find all DOM containers, and render Like buttons into them.
-document.querySelectorAll('.like_button_container')
-  .forEach(domContainer => {
-    // Read the comment ID from a data-* attribute.
-    const commentID = parseInt(domContainer.dataset.commentid, 10);
-    ReactDOM.render(
-      e(LikeButton, { commentID: commentID }),
-      domContainer
-    );
-  });
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
