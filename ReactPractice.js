7a3bec4dae51da97abd2cb1626d9a96e925c5d9d
DAVID HO <davidhorph@hotmail.com>
2018-07-19 12:56:22 -0500

    ...

--- ReactPractice.js
+++ ReactPractice.js
@@ -21,4 +21,12 @@ class LikeButton extends React.Component {
       'Like'
     );
   }
-}
+} 
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDom.render(e(LikeButton), domContainer);
+
+
+
+
+
